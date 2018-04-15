c87bb919ecead88feed451d072c0881e76e12999
ulrich <panoramadata@gmail.com>
2018-04-15 12:22:14 -0400

    QUick notes and ignore db

--- Stats.R
+++ Stats.R
@@ -1,9 +1,16 @@
 library(DBI)
 library(magrittr)
-con <-dbConnect(RSQLite::SQLite(), ".genderify.db")
+con <-dbConnect(RSQLite::SQLite(), "genderify.db")
 
 dbListTables(con)
 
 dbReadTable(con, "artists") %>% View
 
 dbReadTable(con, "artists")$gender %>% table %>% plot
+
+
+# Each individual member of a group is also in the database
+# set is_group == 0 for example
+
+# Ignore lead gender
+
